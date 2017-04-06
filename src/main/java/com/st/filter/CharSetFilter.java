e0f69b4cc2a54a2aa450438be446f424ed9d2cbf
Lt-grint <1598552894@qq.com>
2017-04-06 10:34:00 +0800

    修复CharSetFileter.java,解决中文乱码的问题

--- src/main/java/com/st/filter/CharSetFilter.java
+++ src/main/java/com/st/filter/CharSetFilter.java
@@ -2,6 +2,8 @@ package com.st.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -13,8 +15,10 @@ public class CharSetFilter implements Filter {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-
-        req.setCharacterEncoding(myEncoding);
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        request.setCharacterEncoding(myEncoding);
+        response.setCharacterEncoding(myEncoding);
         chain.doFilter(req, resp);
     }
 
