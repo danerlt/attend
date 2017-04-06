ca1cebe2a3981fb34b8ba696af03de461469f9eb
Lt-grint <1598552894@qq.com>
2017-04-06 10:32:57 +0800

    添加app.js,完成根据用户id查看日志的功能

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,54 @@
+/**
+ * Created by tao on 2017/4/5 0005.
+ */
+function getCookie(Name) {
+    var search = Name + "="//查询检索的值
+    var returnvalue = "";//返回值
+    if (document.cookie.length > 0) {
+        sd = document.cookie.indexOf(search);
+        if (sd!= -1) {
+            sd += search.length;
+            end = document.cookie.indexOf(";", sd);
+            if (end == -1)
+                end = document.cookie.length;
+            //unescape() 函数可对通过 escape() 编码的字符串进行解码。
+            returnvalue=unescape(document.cookie.substring(sd, end))
+        }
+    }
+    return returnvalue;
+}
+
+function findLog(uid) {
+        $.ajax({
+                type:"POST",
+                url:"/attend/findlog",
+                dataType:"json",
+                data:{
+                    uid:getCookie("Id")
+                },
+                success:function (data) {
+                    var uname = '<%=session.getAttribute("uname")%>';
+                    alert(uname);
+                    conlose.log(uname);
+                    $('#table').bootstrapTable({
+                        data: data
+                    })
+                }
+            }
+        );
+    }
+
+$(document).ready(function(){
+    $('#datetimepicker').datetimepicker({
+        format: 'yyyy-MM-dd',
+        language: 'cn',
+        pickDate: true,
+        pickTime: true,
+        hourStep: 1,
+        minuteStep: 15,
+        secondStep: 30,
+        inputMask: true
+    });
+
+})
+
